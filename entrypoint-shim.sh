b0ba3bf05d9a9631b7258b74de16f8de5fd206a7
Chanda Mulenga <dev@stctheproducer.com>
2024-06-16 19:48:46 +0200

    refactor: remove tls domains dependency

--- entrypoint-shim.sh
+++ entrypoint-shim.sh
@@ -5,12 +5,13 @@ if [ ! -z "$CA_FINGERPRINT" ] && [ ! -z "$CA_URL" ]; then
   step ca bootstrap --ca-url $CA_URL \
     --fingerprint $CA_FINGERPRINT --install -f
   step ca root $TLS_CA_CERT_LOCATION
-  export LEGO_CA_CERTIFICATES="$TLS_CA_CERT_LOCATION"
 fi
 
-/usr/local/bin/cert-enroller.sh
-if [ "$?" -eq 0 ]; then
-  /usr/local/bin/cert-renewer.sh &
+if [ ! -z "$TLS_DOMAINS" ]; then
+  /usr/local/bin/cert-enroller.sh
+  if [ "$?" -eq 0 ]; then
+    /usr/local/bin/cert-renewer.sh &
+  fi
 fi
 
 /usr/local/bin/docker-entrypoint.sh "$@"
